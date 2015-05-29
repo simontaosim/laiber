8ab99c978b527ef44d1e4174c43b49a3aba98123
wenjiangtao <wenjiangtao@outlook.com>
2015-05-29 17:28:33 +0800

    Fixed post bugs

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,19 +12,19 @@ class Post < ModelBase
 
 	def self.GetPosts(limit = nil)
 		limit = nil ? -1 : limit
-		return Post.desc(:created_at).limit(limit)
+		return Post.desc(:created_at).where(:post_parent => nil).limit(limit)
 	end
 
 	def self.GetPostsForTop(topPostId, limit = nil)
 		limit = nil ? -1 : limit
 		topPost = Post.find(topPostId)
-		return Post.desc(:created_at).where(:created_at > topPost[:created_at]).limit(limit)
+		return Post.desc(:created_at).where(:post_parent => nil).where(:created_at > topPost[:created_at]).limit(limit)
 	end
 
 	def self.GetPostsForBottom(bottomPostId, limit = nil)
 		limit = nil ? -1 : limit
 		bottomPost = Post.find(bottomPostId)
-		return Post.desc(:created_at).where(:created_at < bottomPost[:created_at]).limit(limit)
+		return Post.desc(:created_at).where(:post_parent => nil).where(:created_at < bottomPost[:created_at]).limit(limit)
 	end
 
 	def self.GetPostsFromParentPost(parentPostId, limit = nil)
