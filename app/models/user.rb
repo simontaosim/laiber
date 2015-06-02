6c75660af8eca75c2a8438565c00ecb6f3ce6be8
simontaosim <xsqfeather@126.com>
2015-06-02 13:43:07 +0800

    123

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,4 @@
 class User < ModelBase
-<<<<<<< HEAD
-  include Mongoid::Document
-  attr_accessor :password, :password_confirmation
-  include Mongoid::Timestamps
-  has_many :user_sessions,  dependent: :destroy
-  has_many :posts, dependent: :destroy
-  has_many :sign_tokens, dependent: :destroy
-  has_many :followers, dependent: :destroy
-  field :name, type: String
-  field :email, type: String
-  field :mobile, type: String
-  field :screct_pass, type: String
-  field :invite_code, type: String
-  
-  validates_confirmation_of :password
-  validates :password, confirmation: true
-  validates_length_of       :name,    :within => 3..100
-  validates_uniqueness_of   :name,    :case_sensitive => false, :massage => '用户名已经存在'
-=======
 	include Mongoid::Document
 	attr_accessor :password, :password_confirmation
 	include Mongoid::Timestamps
@@ -25,12 +6,12 @@ class User < ModelBase
 	has_many :posts, dependent: :destroy
 	has_many :sign_tokens, dependent: :destroy
 	has_many :post_favors, dependent: :destroy
+	has_many :followers, dependent: :destroy
 	field :name, type: String
 	field :email, type: String
 	field :mobile, type: String
 	field :screct_pass, type: String
 	field :invite_code, type: String
->>>>>>> 14acdf0d0e349d51cda1c3fc34171929fc94becf
 
 	validates_confirmation_of :password
 	validates :password, confirmation: true
