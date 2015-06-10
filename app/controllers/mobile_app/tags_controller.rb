534531a6d2f2461f261866858ef5756fa37eb4eb
WenJiangtao-ULaiber-PC-0 <wenjiangtao@outlook.com>
2015-06-10 11:00:45 +0800

    ModelBase.Get

---
+++ app/controllers/mobile_app/tags_controller.rb
@@ -0,0 +1,9 @@
+class MobileApp::TagsController < ApplicationController
+	use Rack::Cors do
+		allow do
+			origins '*', 'null'
+			resource '*', :headers => :any, :methods => [:get, :post]
+		end
+	end
+	skip_before_filter :verify_authenticity_token, only: [:get]
+end
--- app/models/concerns/model_base.rb
+++ app/models/concerns/model_base.rb
@@ -1,4 +1,9 @@
 class ModelBase
+	def self.Get(limit = nil)
+		limit = limit ? limit : -1;
+		self.desc(:created_at).limit(limit)
+	end
+
 	def getId
 		return JSON.parse(self.id.to_json)["$oid"]
 	end
