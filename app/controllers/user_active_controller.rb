e4909da4cbbe84dc785c0ad09d9a14e1fc617ebc
simontaosim <xsqfeather@126.com>
2015-06-14 01:47:40 +0800

    关注和取消关注

--- app/controllers/user_active_controller.rb
+++ app/controllers/user_active_controller.rb
@@ -13,13 +13,23 @@ skip_before_filter :verify_authenticity_token, only: [:collect_post]
   end
 
   def collect_post
-  	if params[:user_id] and params[:post_id]
-  		@post_favor = PostFavor.NewPostFavor(params[:user_id], params[:post_id])
-  		@msg = '帖子收藏成功！'
-  	else
-  		@msg = '帖子收藏失败！'
-  	end
-  	render plain: @msg
+    @user_session = nil
+    if session[:progress]
+      @user_session = UserSession.find(session[:progress].fetch("_id").fetch("$oid"))
+    end
+    if @user_session
+      if params[:user_id] and params[:post_id]
+        @post_favor = PostFavor.NewPostFavor(@user_session.user.id, params[:post_id])
+        @msg = '帖子收藏成功！'
+      else
+        @msg = '帖子收藏失败！'
+      end
+      
+    else
+      @msg = '请先登录'
+    end
+    render plain: @msg
+  	
   end
 
   def cancel_collect_post
@@ -35,12 +45,49 @@ skip_before_filter :verify_authenticity_token, only: [:collect_post]
   	else
   	     @msg = '异常'
   	end
-  	if params[:user_id] and params[:post_id]
-  			PostFavor.where(user_id: params[:user_id], favor_post_id: params[:post_id]).destroy
-  			@msg = '已取消收藏'
-  	end
+    @user_session = nil
+    if session[:progress]
+      @user_session = UserSession.find(session[:progress].fetch("_id").fetch("$oid"))
+    end
+    if @user_session
+      if params[:user_id] and params[:post_id]
+        PostFavor.where(user_id: @user_session.user.id, favor_post_id: params[:post_id]).destroy
+        @msg = '已取消收藏'
+      end
+    else
+      @msg = "请先登录"
+      
+    end
     render plain: @msg
+  end#end of function
 
-  	
+  def to_follow
+    @msg = '内部错误'
+    if params[:user_id]
+      @follower = Follower.new
+      @follower.follower_id = params[:user_id]
+
+      if @follower.save
+        @msg = '关注成功'
+      else
+        @msg = '关注失败，烦请重试!'
+      end
+    end
+    render plain: @msg
+    
   end
+
+  def to_cancel_follow
+    @msg = '内部错误'
+    if params[:user_id]
+      @follower = Follower.find(params[:user_id])
+      if @follower.destroy
+        @msg = '取消关注成功！'
+      end
+    else
+      @msg = '取消关注失败，烦请重试！'
+    end
+    render plain: @msg
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'user_active/to_follow'
   get 'user_active/cancel_collect_post'
   get 'idea_store/show_post'
   post 'user_active/collect_post'
