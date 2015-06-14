651c650c85c4c39d81ddbbaafb4d583b3f594212
simontaosim <xsqfeather@126.com>
2015-06-14 16:04:37 +0800

    123

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,7 +7,7 @@ class WelcomeController < ApplicationController
 
   def show_posts
     @title = 'Laiber——改变世界的愿景'
-    @posts = Post.where(:title.gt => '' ).desc(:created_at).pluck(:id, :title, :image_item_ids, :created_at, :tag_ids)
+    @posts = Post.GetRootPosts(15).pluck(:id, :title, :image_item_ids, :created_at, :tag_ids)
     @post = Post.new
     
     if session[:progress]
